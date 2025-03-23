16c7ea07edf1ece2f5a8754c6c59860dced4d1ab
Pasxa1984 <pasxakarras@gmail.com>
2025-03-23 22:21:17 +0200

    Add files via upload

---
+++ Calculator/Calculator.py
@@ -0,0 +1,17 @@
+a=input()
+b=input()
+
+a1=int(a)
+b1=int(b)
+
+z1=a1+b1
+z2=a1-b1
+z3=a1*b1
+z4=a1/b1
+z5=a1%b1
+
+print("a1+b1=",z1)
+print("a1-b1=",z2)
+print("a1*b1=",z3)
+print("a1/b1=",z4)
+print("a1%b1=",z5)
---
+++ Calculator/Readme.md
@@ -0,0 +1,2 @@
+a simple calculator using Python capable of calculating and printing the answers to 	Addition,Subtraction,Multiplication,Division,Modulus.
+You need first to type two numbers and then run the program
